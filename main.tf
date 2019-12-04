e666705994b600b1c663567a91b44d1887657146
Johannes Kastl <kastl@b1-systems.de>
2019-12-04 20:45:59 +0100

    main.tf: fix indentation

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "openstack_blockstorage_volume_v2" "volumes" {
 }
 
 resource "openstack_compute_volume_attach_v2" "volume_associations" {
-  count = var.desired_number
+  count       = var.desired_number
   instance_id = var.instances_to_attach_to[count.index].id
-  volume_id = openstack_blockstorage_volume_v2.volumes[count.index].id
+  volume_id   = openstack_blockstorage_volume_v2.volumes[count.index].id
 }
